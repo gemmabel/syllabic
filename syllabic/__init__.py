2975aa59d44a306099e392eea367cb74b9a91920
Arturo CURIEL <curieldaz@gmail.com>
2018-02-26 02:45:07 -0600

    Corrected statistics calculation

--- syllabic/__init__.py
+++ syllabic/__init__.py
@@ -1,127 +1,15 @@
 # -*- coding:utf-8 -*-
 
 import os
-
-from cleaner import StoryParser
+import sys
 from syllabic import SyllableStatistics
-from syllabicator import Silabicador
-from ngram import BiGram
-from ceele import CeeleGenerator
-from classify_texts import TextClassifier
 
+if len(sys.argv) < 2: 
+    raise Exception("Usage: python %s PATH_CORPUS_FOLDER" % sys.argv[0].split("/")[-1])
 
-source_dir = "../corpus/raw/"
-source_dir_ceele = "../corpus/ceele"
-res_dir = "../corpus/clean/"
-res_dir_ceele = "../corpus/ceele_clean/"
-generate_corpus = False
-generate_ceele = False
+if not os.path.exists(sys.argv[1]):
+    raise OSError("The provided path does not exist")
+elif not os.path.isdir(sys.argv[1]):
+    raise TypeError("PATH_CORPUS_FOLDER must be a folder")
 
-if generate_corpus: # Generate corpus of children stories
-    story_parser = StoryParser() # Get the corpus parser 
-    for root, _, files in os.walk(source_dir): # Read all raw files
-        for fl in files:
-            with open(root + "/" + fl, "r") as f:
-                try:
-                    content = f.read()
-                except:
-                    continue
-            
-            # Parse the content of the current file
-            story_parser.feed(content)
-            
-            # Save results in a "clean" folder
-            res_path = root.replace("raw", "clean")
-            if "." in fl: # no es folder
-                temp_fname = "".join(fl.split(".")[0:-1]) + ".txt"
-                res_path += temp_fname
-            elif "." in root:
-                res_path = "./" + "".join(res_path.split(".")[0:-1]
-                                          ) + "-" + fl + ".txt"
-            
-            with open(res_path, "w") as f:
-                f.write(story_parser.title)
-                for line in story_parser.result:
-                    f.write(" ")
-                    f.write(line)
-                f.flush()
-                
-            # Reset the parser before continuing
-            story_parser.reset()
-
-if generate_ceele:
-    celegent = CeeleGenerator(source_dir_ceele,
-                              res_dir_ceele)
-
-syl = Silabicador()
-result = syl("querida")
-print("%s" % result)
-result = syl("alquimista")
-print("%s" % result)
-result = syl("guey")
-print("%s" % result)
-result = syl("cayn")
-print("%s" % result)
-result = syl("adherir")
-print("%s" % result)
-result = syl("hembra")
-print("%s" % result)
-result = syl("perro")
-print("%s" % result)
-result = syl("llevar")
-print("%s" % result)
-result = syl("carretera")
-print("%s" % result)
-result = syl("chicharrón")
-print("%s" % result)
-result = syl("transgredir")
-print("%s" % result)
-result = syl("instrucción")
-print("%s" % result)
-result = syl("extrañamiento")
-print("%s" % result)
-result = syl("entrega")
-print("%s" % result)
-result = syl("espronceda")
-print("%s" % result)
-result = syl("institución")
-print("%s" % result)
-result = syl("constitución")
-print("%s" % result)
-result = syl("premiación")
-print("%s" % result)
-result = syl("transgresión")
-print("%s" % result)
-result = syl("playas")
-print("%s" % result)
-result = syl("playa")
-print("%s" % result)
-result = syl("padres")
-print("%s" % result)
-result = syl("calcificación")
-print("%s" % result)
-result = syl("gimnasio")
-print("%s" % result)
-result = syl("acróstico")
-print("%s" % result)
-result = syl("atraen")
-print("%s" % result)
-result = syl("seca")
-print("%s" % result)
-result = syl("salón")
-print("%s" % result)
-result = syl("mano")
-print("%s" % result)
-result = syl("pena")
-print("%s" % result)
-result = syl("supercalifragilisticoespiralidoso")
-print("%s" % result)
-
-# Send the complete corpus to analysis
-#stats = SyllableStatistics(res_dir)
-stats2 = SyllableStatistics(res_dir_ceele)
-classif = TextClassifier(res_dir)
-classif2 = TextClassifier(res_dir_ceele)
-ngram = BiGram(res_dir, with_white_space=True)
-ngram2 = BiGram(res_dir_ceele, with_white_space=True)
-import ipdb;ipdb.set_trace()
+stats = SyllableStatistics(sys.argv[1])
--- syllabic/syllabic.py
+++ syllabic/syllabic.py
@@ -1,149 +1,77 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import re
 import os
 import string
-import nltk
 from syllabicator import Silabicador
 from unidecode import unidecode
 from collections import OrderedDict,Counter
 import numpy as np
-    
+
+
 class Tokenizer(object):
-    
+
     def unique_chars(self, text):
         chars = set(list(text))
         return chars
-    
+
     def remove_punctuation(self, text):
         text = text.replace('\n','')
-        return re.sub(u'[^a-zA-ZáéíóúÁÉÍÓÚñÑ ]', '',text)
-       # spanish_punctuation = r"¡!“-,¿.?,…\xa0\xad–—\n«»”"
-        #return re.sub('[%s]' % re.escape(spanish_punctuation + string.punctuation), ' ', text)
-    
-    def sentences(self, text):
-        spanish_tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
-        return spanish_tokenizer.tokenize(text)
-    
+        return re.sub(u'[^a-zA-Z]', '', unidecode(text))
+
     def tokenize(self, text):
-        temp_text = self.remove_punctuation(text)
-        temp_text = re.sub('\d+', # No numbers
-                           '',
-                           temp_text,
-                           re.UNICODE).lower()
-        temp_text = re.sub(r'[^\w\s]', 
-                           '', 
-                           temp_text, 
-                           re.UNICODE).lower()
-        
-        temp = temp_text.split(" ")
-        while '' in temp:
-            temp.remove('')
-        return temp
-    
-    def tokenize_sentences(self, sentences):
-        res = []
-        for sentence in sentences:
-            res.append(self.tokenize(sentence))
-            
+        tokens = []
+        for token in text.split(" "):
+            if token != "":
+                tokens.append(self.remove_punctuation(token).lower())
+        return tokens
+
 
 class SyllableStatistics(Tokenizer):
-    
+
     def __init__(self, corpus_path, with_accents=False):
 
-        self.pat = []
-        self.freqs = {}
-        self.readable = {}
-        self.syllabificator = Silabicador()
-        
+        self.syllables = set()
+        self.patterns = set()
+
+        self.pattern_freqs = {}
+        self.syllable_freqs = {}
+
+        self.syllable_probabilities = {}
+        self.pattern_probabilities = {}
+
+        silabicador = Silabicador()
         total_syllables = 0
 
-        for root, _, files in os.walk(corpus_path):
-            for filepath in files:
-                
-                with open(root + "/" + filepath, "r") as f:
+        for filepath in os.listdir(corpus_path):
+            if not os.path.isdir(corpus_path + "/" +  filepath):
+                with open(corpus_path + "/" + filepath, "r", encoding="utf8") as f:
                     content = f.read()
-                
                 tokens = self.tokenize(content)
-               # print(tokens)
-                sentences = self.sentences(content) #####
                 for token in tokens:
-                    #print(token)
-                    result, partiall_stuff = self.syllabificator(token)
-                    partiall_stuff = encontrar_patrones(result)
-                    print(result,partiall_stuff) #imprime palabra dividida y patron
-                    self.pat.append(tuple(partiall_stuff))
-                    #print(result) ###### para dividir en silabas
-                    
+                    syllables, patterns = silabicador(token)
+                    total_syllables += len(syllables)
+                    self.syllables = self.syllables.union(syllables)
+                    self.patterns = self.patterns.union(patterns)
+                    for syllable in syllables:
+                        self.syllable_freqs[syllable] = 1 + \
+                                self.syllable_freqs.get(syllable, 0)
+                    for pattern in patterns:
+                        self.pattern_freqs[pattern] = 1 + \
+                                self.pattern_freqs.get(pattern, 0)
 
-                    #print(patrones_silabicos)
-                    #print(partiall_stuff) ###### patronesobj
-#                     try:
-#                         print("-".join([str(res) for res in result]))
-#                     except:
-#                         import ipdb;ipdb.set_trace()
-#                         exit(0)
-                    for syllable in result:
-                        syllable = str(syllable)
-                        if not with_accents:
-                            syllable = unidecode(syllable)
-                            
-                        try:
-                            self.freqs[syllable] += 1
-                        except:
-                            self.freqs[syllable] = 1
-                        total_syllables += 1
-                #print(filepath)
-        
-        self.freqs = OrderedDict(sorted(self.freqs.items(), 
-                                        key=lambda x:x[1],
-                                        reverse=True)
-                                 )
-        
-        self.readable = OrderedDict(sorted(self.readable.items(),
-                                           key=lambda x:x[1],
-                                           reverse=True)
-                                    )
-        
         # Calculate probabilities
-        probabilities = {}
-        for syl, freq in self.freqs.items():
-            probabilities[syl] = freq/total_syllables
-            
-        self.probabilities = np.array(sorted(probabilities.items()))
-        self.syls = np.array(self.probabilities[:,0])
-        self.proba = np.array(self.probabilities[:,1], dtype=np.float64)
-        self.count = Counter(self.pat)
-    
-    
-    def generate_samples(self, n):
-        return np.random.choice(self.syls,
-                                n,
-                                p=self.proba)
+        for syllable, freq in self.syllable_freqs.items():
+            self.syllable_probabilities[syllable] = freq / total_syllables
 
-def encontrar_patrones(palabra_silabicada):
-    patrones = []
-    for silaba in palabra_silabicada:
-        patron = ""
-        if silaba.lower() not in ['ya', 'ye', 'yi', 'yo','yu']:
-            silaba=silaba.replace('y','#', 1)
-                                  
-        for letra in silaba:
-            if letra.lower() in "aeiouáéíóúü":
-                patron += "V"
-            elif letra.lower() in '#':
-                patron += "V"
-        
-            else: # es una consonante o una "y", hay que revisar
-                patron += "C"
-        patrones.append(patron)
-    return patrones
+        for pattern, freq in self.pattern_freqs.items():
+            self.pattern_probabilities[pattern] = freq / total_syllables
 
-palabra_silabicada = ['ha', 'ya']
-patrones_silabicos = encontrar_patrones(palabra_silabicada)
-print(patrones_silabicos)
-obj = SyllableStatistics(r'/home/diana/Escritorio/IPERSPICUIDAD/4.normal/textos/x')
-#print(obj.count)
-#print(obj.freqs)
-#obj = SyllableStatistics(r'E:\IPERSPICUIDAD\inflez\5.muydificil\\')
-##corpus_path= (r'E:\IPERSPICUIDAD\1muy fácil\textos\\')441
+        self.pattern_probabilities = np.array(sorted(
+                                            self.pattern_probabilities.items()
+                                                    )
+                                            )
+        self.syllable_probabilities = np.array(sorted(
+                                            self.syllable_probabilities.items()
+                                                    )
+                                            )
