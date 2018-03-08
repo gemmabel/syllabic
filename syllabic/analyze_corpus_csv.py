5c24ecea7ee7031ad7a268e807f0941679626546
Arturo CURIEL <curieldaz@gmail.com>
2018-03-07 23:34:25 -0600

    Testing

--- syllabic/analyze_corpus_csv.py
+++ syllabic/analyze_corpus_csv.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from scipy.stats import chi2_contingency, chisquare, ttest_ind
 
 inputfiles = ["../results/syllable.csv",
               "../results/sylpattern.csv",
@@ -7,4 +8,12 @@ inputfiles = ["../results/syllable.csv",
              ]
 for path in inputfiles:
     df = pd.read_csv(path, index_col=0)
+    obs = df.iloc[:-1, :-1]
+    ttest_df = pd.DataFrame(columns=obs.columns)
+    for row in obs.columns:
+        for column in obs.columns:
+            ttest_ind()
+            ttest_df[column].loc[row] = 
+    chi2, p_chi2, dof, expected = chi2_contingency(obs)
+    chisq, p_chisq = chisquare(obs, expected)
     import ipdb;ipdb.set_trace()
